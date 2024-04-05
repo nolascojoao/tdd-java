dcde0882e25fc4a532c0bf582fb9453aed3c4965
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-05 20:44:18 -0300

    chapter03: add test for null argument exception

--- chapter03/src/test/java/com/nolascojoao/tdd/ReverseStringTest.java
+++ chapter03/src/test/java/com/nolascojoao/tdd/ReverseStringTest.java
@@ -1,31 +1,47 @@
 package com.nolascojoao.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ReverseStringTest {
 
+	private ReverseString reverse;
+
+	@BeforeEach
+	public void setUp() {
+		reverse = new ReverseString();
+	}
+
 	@ParameterizedTest
 	@MethodSource("getString")
 	public void shouldReturnReverseStringWhenMethodCalled(String input, String expected) {
-		String reverse = ReverseString.reverse(input);
+		String reversed = reverse.reverse(input);
 
-		assertEquals(expected, reverse);
+		assertEquals(expected, reversed);
 	}
-	
-	public static List<Object[]> getString() {
-		return Arrays.asList(new Object[][] {
-			{"a", "a"},
-			{"hello", "olleh"},
-			{"hello world", "dlrow olleh"},
-			{"", ""},
-			{"@#1", "1#@"}
+
+	@Test
+	public void shouldThrowIllegalArgumentExceptionWhenInputIsNull() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			reverse.reverse(null);
 		});
+	}
+
+	public static List<Object[]> getString() {
+		return Arrays.asList(new Object[][] { 
+			{ "a", "a" }, 
+			{ "hello", "olleh" }, 
+			{ "hello world", "dlrow olleh" },
+			{ "", "" }, { "@#1", "1#@" } 
+				});
 	}
 
 }
