e705dd9fb6b08ad5569e960d4bc6d76d712cbfc5
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-05 20:43:41 -0300

    chapter03: add argument checking into reverse method

--- chapter03/src/main/java/com/nolascojoao/tdd/ReverseString.java
+++ chapter03/src/main/java/com/nolascojoao/tdd/ReverseString.java
@@ -5,7 +5,15 @@ import java.util.List;
 
 public class ReverseString {
 
-	public static String reverse(String s) {
+	public ReverseString() {
+
+	}
+
+	public String reverse(String s) {
+		if (s == null) {
+			throw new IllegalArgumentException("String cannot be nulll");
+		}
+
 		List<String> tempArray = new ArrayList<String>(s.length());
 		for (int i = 0; i < s.length(); i++) {
 			tempArray.add(s.substring(i, i + 1));
