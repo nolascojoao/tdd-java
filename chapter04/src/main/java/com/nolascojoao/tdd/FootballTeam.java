4c021ab7892368ea42db77ea393a42122f9e5e68
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-07 19:55:33 -0300

    chapter04: fix domain class to make invalid values test pass

--- chapter04/src/main/java/com/nolascojoao/tdd/FootballTeam.java
+++ chapter04/src/main/java/com/nolascojoao/tdd/FootballTeam.java
@@ -1,13 +1,17 @@
 package com.nolascojoao.tdd;
 
 public class FootballTeam {
-	
+
 	private int gamesWon;
-	
+
 	public FootballTeam(int gamesWon) {
+		if (gamesWon < 0) {
+			throw new IllegalArgumentException("input value cannot be negative");
+		}
+
 		this.gamesWon = gamesWon;
 	}
-	
+
 	public int getGamesWon() {
 		return gamesWon;
 	}
