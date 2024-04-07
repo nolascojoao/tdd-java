6f0596aff9ee54a028fed8e6d27ff9ceec24db29
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-07 18:50:46 -0300

    chapter04: refactor FootballTeamTest class to red-green-refactor cycle

--- chapter04/src/test/java/com/nolascojoao/tdd/FootballTeamTest.java
+++ chapter04/src/test/java/com/nolascojoao/tdd/FootballTeamTest.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class FootballTeamTest {
-	
+
+	private static final int THREE_GAMES_WON = 3;
+
 	@Test
 	public void constructorShouldSetGamesWon() {
-		FootballTeam team = new FootballTeam(3);
-		assertEquals(3, team.getGamesWon());
+		FootballTeam team = new FootballTeam(THREE_GAMES_WON);
+		assertEquals(THREE_GAMES_WON, team.getGamesWon());
 	}
 
 }
