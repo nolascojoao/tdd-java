c82693be23b8c13173843ba06c9a5dbab93f3697
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-07 18:47:19 -0300

    chapter04: fix FootballTeam class(domain) to make test pass

--- chapter04/src/main/java/com/nolascojoao/tdd/FootballTeam.java
+++ chapter04/src/main/java/com/nolascojoao/tdd/FootballTeam.java
@@ -2,12 +2,14 @@ package com.nolascojoao.tdd;
 
 public class FootballTeam {
 	
+	private int gamesWon;
+	
 	public FootballTeam(int gamesWon) {
-		
+		this.gamesWon = gamesWon;
 	}
 	
 	public int getGamesWon() {
-		return 0;
+		return gamesWon;
 	}
 
 }
