1a1ef3cabec3d10b4700343388287d069fceef0e
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-08 18:48:12 -0300

    chapter04: refactor isValid method with Regex

--- chapter04/src/main/java/com/nolascojoao/tdd/PasswordValidator.java
+++ chapter04/src/main/java/com/nolascojoao/tdd/PasswordValidator.java
@@ -1,8 +1,11 @@
 package com.nolascojoao.tdd;
 
+import java.util.regex.Pattern;
+
 public class PasswordValidator {
 
 	private String password;
+	private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
 
 	public PasswordValidator() {
 
@@ -17,46 +20,7 @@ public class PasswordValidator {
 	}
 
 	public boolean isValid(String password) {
-		// check if the password has at least 8 characters
-		if (password.length() < 8) {
-			return false;
-		}
-
-		// check if the password contains at least one uppercase letter
-		boolean hasUpperCase = false;
-		for (char c : password.toCharArray()) {
-			if (Character.isUpperCase(c)) {
-				hasUpperCase = true;
-				break;
-			}
-		}
-		if (!hasUpperCase) {
-			return false;
-		}
-
-		// check if the password contains at least one lowercase letter
-		boolean hasLowerCase = false;
-		for (char c : password.toCharArray()) {
-			if (Character.isLowerCase(c)) {
-				hasLowerCase = true;
-				break;
-			}
-		}
-
-		// check if the password contains at least one digit
-		boolean hasDigit = false;
-		for (char c : password.toCharArray()) {
-			if (Character.isDigit(c)) {
-				hasDigit = true;
-				break;
-			}
-		}
-		if (!hasDigit) {
-			return false;
-		}
-
-		// password it is considered valid if passes all checks
-		return true;
+		return Pattern.matches(PASSWORD_PATTERN, password);
 	}
 
 }
