04327258e5808145722d59d9adb376e9723a3e69
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-08 17:20:47 -0300

    chapter04: add comparison tests

--- chapter04/src/test/java/com/nolascojoao/tdd/FootballTeamTest.java
+++ chapter04/src/test/java/com/nolascojoao/tdd/FootballTeamTest.java
@@ -49,4 +49,37 @@ public class FootballTeamTest {
 		assertTrue(team instanceof Comparable, "FootballTeam should implement Comparable");
 	}
 
+	@Test
+	public void teamsWithMoreMatchesWonShouldBeGreater() {
+		FootballTeam team_2 = new FootballTeam(2);
+		FootballTeam team_3 = new FootballTeam(3);
+
+		assertTrue(team_3.compareTo(team_2) > 0,
+				" team with " + team_3.getGamesWon()
+				+ " games won should be ranked before the team with "
+				+ team_2.getGamesWon() + " games won");
+	}
+
+	@Test
+	public void teamsWithLessMatchesWonShouldBeLesser() {
+		FootballTeam team_2 = new FootballTeam(2);
+		FootballTeam team_3 = new FootballTeam(3);
+
+		assertTrue(team_2.compareTo(team_3) < 0,
+				" team with " + team_2.getGamesWon()
+				+ " games won should be ranked after the team with "
+				+ team_3.getGamesWon() + " games won");
+	}
+	
+	@Test
+	public void teamsWithSameNumberOfMatchesWonShouldBeEqual() {
+		FootballTeam teamA = new FootballTeam(2);
+		FootballTeam teamB = new FootballTeam(2);
+		
+		assertTrue(teamA.compareTo(teamB) == 0,
+				" both teams have won the same number of games: "
+				+ teamA.getGamesWon() + " vs " + teamB.getGamesWon()
+				+ " and should be ranked equal");
+	}
+
 }
